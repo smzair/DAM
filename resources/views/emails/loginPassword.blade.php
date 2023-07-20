92b8c35d732ec725f76d2fcdf9ef13e169523add
SantuSahu <santu.sahu001@gmail.com>
2023-07-20 11:30:44 +0530

    Mail and side bar menu changes

--- resources/views/emails/loginPassword.blade.php
+++ resources/views/emails/loginPassword.blade.php
@@ -1,18 +1,23 @@
 <p>Hello <strong> {{$user['name']}}, </strong> </p>
-<p>
-    <strong> Welcome To ODN Digital</strong>
+<p> <strong> Thanks for trusting ODN as your partner for the Digital Asset Management Journey.</strong></p>
+
+<p> <strong>Detailed below are your login credentials to access your digital assets.</strong></p>
+<p> <strong>Please login by clicking on the URL below.</strong></p>
+<p> <strong>You will be prompted for your login credentials that are mentioned
+subsequently.</strong></p>
+<p> <strong>We recommend you change your password immediately after login using the change password option for enhanced security.</strong></p>
 </p>
-<p>Client - id <strong> {{$user['client_id']}}, </strong> </p>
-<p>Email : <strong> {{$user['email']}}</strong></p> 
+<li>Url: <strong><a href="https://odnconnect.odndigital.com">https://odnconnect.odndigital.com </strong></a></li>
+<li>Email: <strong> {{$user['email']}}</strong></li> 
+
 <p>
-    Your Login password is :         
-    <strong style="color: rgb(31, 107, 12)"> {{$user['password']}} </strong>
+    Your Login password is: <strong style="color: rgb(31, 107, 12)"> {{$user['password']}} </strong>
 </p>
 <p>
     <br><br>
     <strong>Thank You! </strong>
     <br>
-    <strong>ODN.Connect | Innovative Content Creators</strong>
+    <strong>ODN Digital | Shop-Now Content</strong>
     <br><br>
 </p>
 
