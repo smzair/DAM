2e99a79a156ba3d29f6547fefe695a11366abfc7
cpurajesh666 <rajesh302502216009@gmail.com>
2023-07-21 10:51:28 +0530

    allocation panel changes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//get user name
+Route::get('get-company-user-name', [ApiController::class, 'getCompanyUserName']);
+//get catlog wrc api
+Route::get('get-wrc-data-basedon-userid-brandid', [ApiController::class, 'getCatlogWrc']);
+// get brand based on user id
+Route::get('get-brands/{userId}', [ApiController::class, 'getBrands']);
+
